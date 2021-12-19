79a17d441df0c17398a4c001c5d9ffc05beda192
izzy <izzy84075@gmail.com>
2021-12-19 14:37:00 -0800

    Add text display for the text update messages, and try a different name for that first bit in the remote header.

--- sony_md_decode/pd.py
+++ sony_md_decode/pd.py
@@ -43,6 +43,101 @@ class Decoder(srd.Decoder):
 		('errors', 'Errors', (10,)),
 		('warnings', 'Warnings', (11,)),
 	)
+
+	characters = {
+		0x04: "<minidisc icon>, 0x04",
+		0x14: "<music note icon>, 0x14",
+
+		0x20: "' ', space, 0x20",
+		0x21: "!",
+		0x22: "\"",
+		0x23: "#",
+		0x24: "$",
+		0x25: "%",
+		0x26: "&",
+		0x27: "'",
+		0x28: "(",
+		0x29: ")",
+		0x2A: "*",
+		0x2B: "+",
+		0x2C: ",",
+		0x2D: "-",
+		0x2E: ".",
+		0x2F: "/",
+		0x30: "0",
+		0x31: "1",
+		0x32: "2",
+		0x33: "3",
+		0x34: "4",
+		0x35: "5",
+		0x36: "6",
+		0x37: "7",
+		0x38: "8",
+		0x39: "9",
+		0x3A: ":",
+		0x3B: ";",
+		0x3C: "<",
+		0x3D: "=",
+		0x3E: ">",
+		0x3F: "?",
+		0x40: "@",
+		0x41: "A",
+		0x42: "B",
+		0x43: "C",
+		0x44: "D",
+		0x45: "E",
+		0x46: "F",
+		0x47: "G",
+		0x48: "H",
+		0x49: "I",
+		0x4A: "J",
+		0x4B: "K",
+		0x4C: "L",
+		0x4D: "M",
+		0x4E: "N",
+		0x4F: "O",
+		0x50: "P",
+		0x51: "Q",
+		0x52: "R",
+		0x53: "S",
+		0x54: "T",
+		0x55: "U",
+		0x56: "V",
+		0x57: "W",
+		0x58: "X",
+		0x59: "Y",
+		0x5A: "Z",
+
+		0x5F: "_",
+		0x60: "`",
+		0x61: "a",
+		0x62: "b",
+		0x63: "c",
+		0x64: "d",
+		0x65: "e",
+		0x66: "f",
+		0x67: "g",
+		0x68: "h",
+		0x69: "i",
+		0x6A: "j",
+		0x6B: "k",
+		0x6C: "l",
+		0x6D: "m",
+		0x6E: "n",
+		0x6F: "o",
+		0x70: "p",
+		0x71: "q",
+		0x72: "r",
+		0x73: "s",
+		0x74: "t",
+		0x75: "u",
+		0x76: "v",
+		0x77: "w",
+		0x78: "x",
+		0x79: "y",
+		0x7A: "z",
+
+	}
 	
 	def putMessageStart(self, messageStartSample):
 		self.put(messageStartSample, messageStartSample, self.out_ann,
@@ -127,13 +222,13 @@ class Decoder(srd.Decoder):
 		self.put(bitData[3][currentBit][0], bitData[3][currentBit+7][2], self.out_ann,
 			[8, ['Remote', 'R']])
 		self.putValueLSBFirst(bitData, currentBit, 8)
-		
+
 		if bitData[3][currentBit+1][3] == 1:
 			self.put(bitData[3][currentBit+1][0], bitData[3][currentBit+1][2], self.out_ann,
-				[3, ['Remote is active']])
+				[3, ['Remote is ready for text']])
 		else:
 			self.put(bitData[3][currentBit+1][0], bitData[3][currentBit+1][2], self.out_ann,
-				[6, ['Remote is NOT active']])
+				[6, ['Remote is NOT ready for text']])
 		if bitData[3][currentBit+4][3] == 1:
 			self.put(bitData[3][currentBit+4][0], bitData[3][currentBit+4][2], self.out_ann,
 				[3, ['Remote HAS data to send', 'RY']])
@@ -146,6 +241,10 @@ class Decoder(srd.Decoder):
 		else:
 			self.put(bitData[3][currentBit+7][0], bitData[3][currentBit+7][2], self.out_ann,
 				[6, ['Remote NOT Present', 'RNP']])
+		
+		#if (bitData[3][currentBit+7][3] == 1) and (bitData[3][currentBit+1][3] == 0):
+			#self.put(bitData[3][currentBit][0], bitData[3][currentBit+7][2], self.out_ann,
+				#[11, ['Remote present but not active!']])
 	
 	def putPlayerHeader(self, bitData, currentBit):
 		self.put(bitData[3][currentBit][0], bitData[3][currentBit+7][2], self.out_ann,
@@ -167,6 +266,16 @@ class Decoder(srd.Decoder):
 				[6, ['Player does NOT cede the bus to remote after header', 'PDB']])
 		self.put(bitData[3][currentBit+7][0], bitData[3][currentBit+7][2], self.out_ann,
 			[3, ['Player Present']])
+	
+	def putLCDCharacter(self, bitData, currentBit, value):
+		if value in self.characters:
+			self.put(bitData[3][currentBit][0], bitData[3][currentBit+7][2], self.out_ann,
+				[3, [self.characters[value]]])
+		else:
+			self.put(bitData[3][currentBit][0], bitData[3][currentBit+7][2], self.out_ann,
+				[3, ['Unknown character']])
+			self.put(bitData[3][currentBit][0], bitData[3][currentBit+7][2], self.out_ann,
+				[10, ['Unknown character']])
 
 	def expandPlayerDataBlock(self, bitData, currentBit, packetType):
 		self.put(bitData[3][currentBit][0], bitData[3][currentBit+7][2], self.out_ann,
@@ -431,35 +540,49 @@ class Decoder(srd.Decoder):
 					[3, ['Non-final segment?']])
 				self.put(bitData[3][currentBit+24][0], bitData[3][currentBit+31][2], self.out_ann,
 					[9, ['String position "1"?']])
+				self.putLCDCharacter(bitData, currentBit+24, self.values[5])
 				self.put(bitData[3][currentBit+32][0], bitData[3][currentBit+39][2], self.out_ann,
 					[9, ['String position "2"?']])
+				self.putLCDCharacter(bitData, currentBit+32, self.values[6])
 				self.put(bitData[3][currentBit+40][0], bitData[3][currentBit+47][2], self.out_ann,
 					[9, ['String position "3"?']])
+				self.putLCDCharacter(bitData, currentBit+40, self.values[7])
 				self.put(bitData[3][currentBit+48][0], bitData[3][currentBit+55][2], self.out_ann,
 					[9, ['String position "4"?']])
+				self.putLCDCharacter(bitData, currentBit+48, self.values[8])
 				self.put(bitData[3][currentBit+56][0], bitData[3][currentBit+63][2], self.out_ann,
 					[9, ['String position "5"?']])
+				self.putLCDCharacter(bitData, currentBit+56, self.values[9])
 				self.put(bitData[3][currentBit+64][0], bitData[3][currentBit+71][2], self.out_ann,
 					[9, ['String position "6"?']])
+				self.putLCDCharacter(bitData, currentBit+64, self.values[10])
 				self.put(bitData[3][currentBit+72][0], bitData[3][currentBit+79][2], self.out_ann,
 					[9, ['String position "7"?']])
+				self.putLCDCharacter(bitData, currentBit+72, self.values[11])
 			elif self.values[3] == 0x01:
 				self.put(bitData[3][currentBit+8][0], bitData[3][currentBit+15][2], self.out_ann,
 					[3, ['Final segment?']])
 				self.put(bitData[3][currentBit+24][0], bitData[3][currentBit+31][2], self.out_ann,
 					[9, ['String position "1"?']])
+				self.putLCDCharacter(bitData, currentBit+24, self.values[5])
 				self.put(bitData[3][currentBit+32][0], bitData[3][currentBit+39][2], self.out_ann,
 					[9, ['String position "2"?']])
+				self.putLCDCharacter(bitData, currentBit+32, self.values[6])
 				self.put(bitData[3][currentBit+40][0], bitData[3][currentBit+47][2], self.out_ann,
 					[9, ['String position "3"?']])
+				self.putLCDCharacter(bitData, currentBit+40, self.values[7])
 				self.put(bitData[3][currentBit+48][0], bitData[3][currentBit+55][2], self.out_ann,
 					[9, ['String position "4"?']])
+				self.putLCDCharacter(bitData, currentBit+48, self.values[8])
 				self.put(bitData[3][currentBit+56][0], bitData[3][currentBit+63][2], self.out_ann,
 					[9, ['String position "5"?']])
+				self.putLCDCharacter(bitData, currentBit+56, self.values[9])
 				self.put(bitData[3][currentBit+64][0], bitData[3][currentBit+71][2], self.out_ann,
 					[9, ['String position "6"?']])
+				self.putLCDCharacter(bitData, currentBit+64, self.values[10])
 				self.put(bitData[3][currentBit+72][0], bitData[3][currentBit+79][2], self.out_ann,
 					[9, ['String position "7"?']])
+				self.putLCDCharacter(bitData, currentBit+72, self.values[11])
 			else:
 				self.put(bitData[3][currentBit+8][0], bitData[3][currentBit+15][2], self.out_ann,
 					[10, ['UNRECOGNIZED VALUE']])
